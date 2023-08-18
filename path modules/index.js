40cfb1ac77b1f134d42bd16957d8bc49f1a3efe2
Hammad-Asif-Official <034928hammadasif582@gmail.com>
2023-08-18 19:19:02 +0600

    path modules

---
+++ path modules/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const a = path.basename("/hammad/asif/jawad/faraz/index.html");
+console.log(a);
+const b = path.dirname("/hammad/asif/jawad/faraz/index.html");
+console.log(b)
+const c = path.extname("/hammad/asif/jawad/faraz/index.html");
+console.log(c)
