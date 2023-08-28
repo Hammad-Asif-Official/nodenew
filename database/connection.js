90ea5bfb6e2665a4c905149cc763e41d7e2b2bd9
Hammad-Asif-Official <034928hammadasif582@gmail.com>
2023-08-28 16:13:37 +0600

    Multiple Schema

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,28 @@ mongoose.connect("mongodb://127.0.0.1:27017/nodejs").then(() => {
 })
 
 const schema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confpassword: {
+        type: String,
+        required: true,
+    }
+
+
 })
 
 const user = mongoose.model("user", schema);
