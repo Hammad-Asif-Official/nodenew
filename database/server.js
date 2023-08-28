8592165f714d6e23f8c6de29df304c6a7ddb2c40
Hammad-Asif-Official <034928hammadasif582@gmail.com>
2023-08-28 16:29:50 +0600

    node module delete and LOGIN system add

--- database/server.js
+++ database/server.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const app = express();
-const user = require("./connection")
-
+const user = require("./connection");
 
 //post =>data send to database
-
-
-
 
 app.use(express.json());
 app.post("/", async(req, res) => {
     try {
-        const senddata = new user(req.body)
+        const senddata = new user(req.body);
         const savedata = await senddata.save();
-        res.send(savedata)
+        res.send(savedata);
     } catch (error) {
-
         res.status(404).send(error);
     }
-
-})
+});
 
 //get  => get all from database
 
@@ -29,9 +23,8 @@ app.get("/user", async(req, res) => {
         res.send(getdata);
     } catch (error) {
         res.status(404).send(error);
-
     }
-})
+});
 
 // get => get spacific data from database
 
@@ -43,23 +36,33 @@ app.get("/user/:id", async(req, res) => {
     } catch (error) {
         res.status(404).send(error);
     }
-})
+});
+
+// login data
+
+app.post("/login", async(req, res) => {
+    try {
+        const useremail = req.body.email;
+        const getemail = await user.findOne({ email: useremail });
+        res.send(getemail);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
 
 //patch =>update the data in database
 
 app.patch("/update/:id", async(req, res) => {
     try {
         const id = req.params.id;
-        const updatedata = await user.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+        const updatedata = await user.findByIdAndUpdate({ _id: id }, req.body, {
+            new: true,
+        });
         res.send(updatedata);
     } catch (error) {
         res.status(500).send(error);
-
-
     }
-})
-
-
+});
 
 //delete =>delete the data in database
 
@@ -70,13 +73,8 @@ app.delete("/delete/:id", async(req, res) => {
         res.send(deletedata);
     } catch (error) {
         res.status(500).send(error);
-
     }
-})
-
-
-
-
+});
 
 app.listen(3000, () => {
     console.log("server run succesfully");
